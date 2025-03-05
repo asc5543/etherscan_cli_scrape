942c9d8994091ac3baf94bfc6bfdde90a593f12a
asc5543 <asc5543@gmail.com>
2025-03-05 16:46:30 +0800

    Complete the block page parsing.

--- scraper.py
+++ scraper.py
@@ -1,31 +1,87 @@
 from transaction_detail import TransactionDetail
 
 import argparse
+import json
 from collections import defaultdict
 import urllib.request
 
-def get_transaction_detail_by_hash(trans_hash: str) -> TransactionDetail:
-    trans_detail = TransactionDetail()
-    url = r"https://etherscan.io/tx/" + trans_hash
-    print(url)
-    return trans_detail
-
-def get_transaction_list_by_block(block: int, method: str, amount_filter: bool, zero_amount: bool) -> list[str]:
-    """Return the transaction hash list based on the filter."""
-    trans_list = []
-    url = r"https://etherscan.io/txs?block=" + str(block)
-    # Use header to mimic as a browser
+def read_page(url: str) -> str:
+    """Read page and return the raw content"""
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
     req = urllib.request.Request(url, headers=headers)
-    block_page = urllib.request.urlopen(req)
-    content = block_page.read().decode('utf-8')
-    print(content)
+    target_page = urllib.request.urlopen(req)
+    content = target_page.read().decode("utf-8")
+    return content
+
+def get_transaction_detail_by_hash(txhash: str) -> (str, float):
+    """Return the sponsored and gas_price info"""
+    url = r"https://etherscan.io/tx/" + txhash
+    content = read_page(url)
+
+    sponsored = ""
+    gas_price = 0.0
+    return sponsored, gas_price
+
+def get_transaction_list_by_block(block: int, method: str, amount_filter: bool, zero_amount: bool) -> list[TransactionDetail]:
+    """Return the transaction hash list based on the filter."""
+    trans_list = []
+    url = r"https://etherscan.io/txs?block=" + str(block)
+    # Use header to mimic as a browser
+    content = read_page(url)
+    # Can use regex to parse
+    total_trans = int(content.split("A total of ")[1].split(" transactions found")[0])
+    total_page = total_trans // 50 + 1
+
+    for i in range(1, total_page+1):
+        block_url = url + f"&p={i}"
+        if i > 1:
+            content = read_page(block_url)
+        hash_info = content.split("const quickExportTransactionListData = '")[1].split("';")[0]
+        hash_json = json.loads(hash_info)
+        for hash_detail in hash_json:
+            if method and hash_detail['Method'] != method:
+                continue
+            if amount_filter:
+                if zero_amount and hash_detail['Amount'] != '0 ETH':
+                    continue
+                elif not zero_amount and hash_detail['Amount'] == '0 ETH':
+                    continue
+            # Sponsored and gas price does not exist on block page
+            # Need to parse them from transaction details page
+            sponsored, gas_price = get_transaction_detail_by_hash(hash_detail['Txhash'])
+            sender = ""
+            receiver = ""
+            if hash_detail['SenderLable']:
+                sender = f"{hash_detail['SenderLable']}({hash_detail['Sender']})"
+            else:
+                sender = hash_detail['Sender']
+            if hash_detail['ReceiverLable']:
+                receiver = f"{hash_detail['ReceiverLable']}({hash_detail['Receiver']})"
+            else:
+                receiver = hash_detail['Receiver']
+            trans_detail = TransactionDetail(
+                trans_hash = hash_detail['Txhash'],
+                status = hash_detail['Status'],
+                block = block,
+                timestamp = hash_detail['DateTime'],
+                method = hash_detail['Method'],
+                sponsored = sponsored,
+                source = sender,
+                target = receiver,
+                value = f"{hash_detail['Amount']}({hash_detail['Value']})",
+                fee = hash_detail['TxnFee'],
+                gas_price = gas_price,
+            )
+            trans_list.append(trans_detail)
+            print(trans_detail)
+            return trans_list
 
     return trans_list
 
 def main():
+    # Handle the filters
     parser = argparse.ArgumentParser()
     parser.add_argument("--block_range", help="Set the block range. Ex. 100-200 / 500")
     parser.add_argument("--method", help="Set the method filter. Ex. Transfer/Commit Blob")
@@ -33,7 +89,6 @@ def main():
     args = parser.parse_args()
 
     block_start = block_end = 0
-    bool_method_filter = False
     target_method = ""
     bool_amount_filter = False
     zero_amount = False
@@ -46,7 +101,6 @@ def main():
         print("The block range cannot exceed 100!!!")
         return
     if args.method:
-        bool_method_filter = True
         target_method = args.method
     if args.zero_amount:
         bool_amount_filter = True
@@ -54,20 +108,16 @@ def main():
             zero_amount = True
         else:
             zero_amount = False
-    """
-    print(f"Block range: {block_start} - {block_end}")
-    if bool_method_filter:
-        print(f"Method filter: {target_method}")
-    if bool_amount_filter:
-        print(f"Amount Target is zero amount: {zero_amount}")
-    """
 
+    # Parse the transaction hash list by block
+    # Filter out in this stage
     block_details = defaultdict(list)
     for block in range(block_start, block_end+1):
         transaction_list = get_transaction_list_by_block(block, target_method, bool_amount_filter, zero_amount)
         block_details[block] = transaction_list
+    #print(block_details)
 
-
+    # TODO: Dump the information
 
 
 # Get the block range, method, and amount by CLI filter
--- transaction_detail.py
+++ transaction_detail.py
@@ -2,15 +2,42 @@
 
 class TransactionDetail:
     """Records the transaction detail of each transaction."""
-    def __init__(self):
-        self.trans_hash = ""
-        self.status = ""
-        self.block = ""
-        self.timestamp = ""
-        self.action = ""
-        self.sponsored = ""
-        self.source = "" # From
-        self.target = "" # To
-        self.value = ""
-        self.fee = ""
-        self.gas_price = ""
+    def __init__(
+            self,
+            trans_hash: str = "",
+            status: str = "",
+            block: int = 0,
+            timestamp: str = "",
+            method: str = "",
+            sponsored: str = "",
+            source: str = "",
+            target: str = "",
+            value: str = "",
+            fee: float = 0,
+            gas_price: float = 0,
+    ):
+        self.trans_hash = trans_hash
+        self.status = status
+        self.block = block
+        self.timestamp = timestamp
+        self.method = method # Action
+        self.sponsored = sponsored
+        self.source = source # From
+        self.target = target # To
+        self.value = value
+        self.fee = fee
+        self.gas_price = gas_price
+
+    def __str__(self):
+        print_string = (f"Transaction Hash: {self.trans_hash}\n"
+                        f"Status: {self.status}\n"
+                        f"Block: {self.block}\n"
+                        f"Timestamp: {self.timestamp}\n"
+                        f"Transaction Action: {self.method}\n"
+                        f"Sponsored: {self.sponsored}\n"
+                        f"From: {self.source}\n"
+                        f"To: {self.target}\n"
+                        f"Value: {self.value}\n"
+                        f"Transaction Fee: {self.fee}\n"
+                        f"Gas Price: {self.gas_price}")
+        return print_string
